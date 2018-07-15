d1a7dfdb51f79d5d7949c879a5cd0289ae9ac7c0
Martynenko Andey <ya.lergor@yandex.ru>
2018-07-15 22:22:36 +0300

    ip/ping = pong

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def ip():
+    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
+@app.route('/ping')
+def pong():
+    return 'pong'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False
